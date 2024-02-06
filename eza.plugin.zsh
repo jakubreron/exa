d7995473f62efee0d23821f61e3307f92d026a4e
Jakub Reroń <kubareron@gmail.com>
2024-02-06 14:41:51 +0900

    config: disable git integration

--- eza.plugin.zsh
+++ eza.plugin.zsh
@@ -7,16 +7,7 @@ if ! (( $+commands[eza] )); then
 fi
 
 # Create alias override commands using 'eza'
-alias ls='eza --group-directories-first --icons'
-
-# Use the --git flag if the installed version of eza supports git
-# Related to https://github.com/ogham/exa/issues/978
-if eza --version | grep -q '+git';
-then
-	alias ll='ls -lh --git'
-else
-	alias ll='ls -lh'
-fi
-
+alias ls='eza --group-directories-first --icons --no-git'
+alias ll='ls -lh'
 alias la='ll -a'
 alias tree='ll --tree --level=2'
